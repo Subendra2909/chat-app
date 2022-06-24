0f952271681057f89bd834b247c0eeacc47095e5
M Durga Subendra <you@example.com>
2022-06-24 18:16:10 +0530

    completed upto chat top and bottom

--- src/misc/firebase.jsx
+++ src/misc/firebase.jsx
@@ -1,8 +1,12 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+import 'firebase/compat/storage';
 
 const config = {
   apiKey: 'AIzaSyCDuLpOZrOEJD7-1ieYEQ7ITw2rN9YN1yE',
   authDomain: 'chat-web-app-8ef3f.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-8ef3f-default-rtdb.firebaseio.com',
   projectId: 'chat-web-app-8ef3f',
   storageBucket: 'chat-web-app-8ef3f.appspot.com',
   messagingSenderId: '1005494725615',
@@ -10,3 +14,6 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
+export const storage = app.storage();
