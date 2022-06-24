241af3a3a17f95fce0fa55787e5377764edd006f
M Durga Subendra <you@example.com>
2022-06-24 10:09:37 +0530

    initialized firebase project

---
+++ src/misc/firebase.jsx
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCDuLpOZrOEJD7-1ieYEQ7ITw2rN9YN1yE',
+  authDomain: 'chat-web-app-8ef3f.firebaseapp.com',
+  projectId: 'chat-web-app-8ef3f',
+  storageBucket: 'chat-web-app-8ef3f.appspot.com',
+  messagingSenderId: '1005494725615',
+  appId: '1:1005494725615:web:3e03fd1ad90e2cc8a6241a',
+};
+
+const app = firebase.initializeApp(config);
